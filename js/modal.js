ad7d0d3b33a410002e8c785aa09b0a565cec9fa8
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-11-25 18:22:41 +0200

    h

--- js/modal.js
+++ js/modal.js
@@ -1,9 +1,6 @@
 (() => {
   const refs = {
-    
-    
     openModalBtn: document.querySelectorAll("[data-modal-open]"),
-   
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
   };
