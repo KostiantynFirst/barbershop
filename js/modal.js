01b1c867754ce8eb409c9cd57ef691fc9537cc91
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-12-19 11:13:42 +0200

    add mobile menu

--- js/modal.js
+++ js/modal.js
@@ -1,20 +1,14 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelectorAll("[data-modal-open]"),
+    openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
-    backdrop: document.querySelector("[data-modal-backdrop]"),
     modal: document.querySelector("[data-modal]"),
   };
 
-  refs.openModalBtn.forEach(modal => {
-    modal.addEventListener("click", toggleModal);
-  })
-
+  refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
-  refs.backdrop.addEventListener('click', toggleModal)
 
   function toggleModal() {
-    refs.backdrop.classList.toggle("is-hidden");
-
+    refs.modal.classList.toggle("is-hidden");
   }
 })();
